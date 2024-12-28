71d909dc96dc13e9e5e10abefd229b261bc0d82a
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-12-28 12:13:15 +0500

    The files are structured

---
+++ src/pages/HomeworkFive/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function HomeworkFive() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeworkFive
---
+++ src/pages/HomeworkOne/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+function HomeworkOne() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomeworkOne
