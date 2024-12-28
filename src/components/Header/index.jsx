1382a6f9388957475478c26b9a5aad162baec8d5
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-12-28 12:29:47 +0500

    The make part header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './index.css'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className="header">
+        <div className="header_container">
+            <h2>Logo</h2>
+
+            <div className="homeworks">
+                <NavLink className="link" to="/">Homework One</NavLink>
+                <NavLink className="link" to="/homeworkTwo">Homework Two</NavLink>
+                <NavLink className="link" to="/homeworkThree">Homework Three</NavLink>
+                <NavLink className="link" to="/homeworkFour">Homework Four</NavLink>
+                <NavLink className="link" to="/homeworkFive">Homework Five</NavLink>
+            </div>
+        </div>
+    </header>
   )
 }
 
